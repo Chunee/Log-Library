cb5cc305a414d8572f2b6e5324128166444af279
Chunee <lowchunee0305@gmail.com>
2024-06-09 14:36:11 +0800

    added files that use io_uring to write file asynchronously

---
+++ io_context.h
@@ -0,0 +1,57 @@
+#include <liburing.h>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <stdexcept>
+#include <cstring>
+#include <memory>
+#include <string>
+#include <iostream>
+
+#define QUEUE_DEPTH 8
+#define BUFFER_SIZE 4096
+#define BUFFER_GROUP_ID 0
+
+struct io_uring_buf_ring;
+
+class io_context {
+public:
+    io_context();
+
+    io_context(const io_context&) = delete;
+
+    io_context(io_context&&) = delete;
+
+    io_context& operator=(const io_context&) = delete;
+
+    io_context& operator=(io_context&&) = delete;
+
+    ~io_context();
+
+    void write(int, const char*, size_t);
+
+private:
+    void initialize_buffer(const unsigned int, const size_t);
+
+    void register_buffer();
+    
+    void unregister_buffer();
+    
+    void prepare_write_request(int, const char*, size_t, off_t);
+
+    void submit_request();
+
+    void handle_completion();
+
+    void run_event_loop();
+
+    void stop_event_loop();
+
+private:
+    struct io_uring io_uring_;
+    std::thread event_loop_thread_;
+    std::atomic<bool> running_;
+    char buffer[BUFFER_SIZE];
+    std::unique_ptr<io_uring_buf_ring> buffer_ring_;
+    std::vector<std::vector<char>> buffer_list_;
+};
